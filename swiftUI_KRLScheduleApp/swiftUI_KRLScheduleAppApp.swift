b776a670093a09f98a0cb67cf8c1c96431991e2f
dira sanjaya wardana <dirawardana.dw@gmail.com>
2024-05-02 09:57:02 +0700

    Initial Commit

---
+++ swiftUI_KRLScheduleApp/swiftUI_KRLScheduleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_KRLScheduleAppApp.swift
+//  swiftUI_KRLScheduleApp
+//
+//  Created by MACBOOK PRO on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_KRLScheduleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
